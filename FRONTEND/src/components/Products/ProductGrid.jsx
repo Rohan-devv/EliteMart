b38a7d1adf46d8320e9fe609b9c273f61b818db7
Rohan-devv <devv112004@gmail.com>
2025-07-20 23:52:31 +0530

    h4ruu

--- FRONTEND/src/components/Products/ProductGrid.jsx
+++ FRONTEND/src/components/Products/ProductGrid.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const ProductGrid = () => {
+const ProductGrid = ({products}) => {
   return (
-    <div>ProductGrid</div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+  {products.map((product, index) => (
+    <Link key={index} to={`/product/${product._id}`} className="block">
+      <div className="bg-white p-4 rounded-lg">
+        <div className="w-full h-96 mb-4">
+          <img
+            src={product.images[0].url}
+            alt={product.images[0].altText || product.name}
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </div>
+      </div>
+    </Link>
+  ))}
+</div>
+
   )
 }
 
