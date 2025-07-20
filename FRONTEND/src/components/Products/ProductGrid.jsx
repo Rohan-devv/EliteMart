afe2909009f189dfa29e941864a87b4f2394cde8
Rohan-devv <devv112004@gmail.com>
2025-07-21 00:52:18 +0530

    kjernkn

--- FRONTEND/src/components/Products/ProductGrid.jsx
+++ FRONTEND/src/components/Products/ProductGrid.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const ProductGrid = ({products}) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
   {products.map((product, index) => (
     <Link key={index} to={`/product/${product._id}`} className="block">
       <div className="bg-white p-4 rounded-lg">
@@ -13,18 +13,16 @@ const ProductGrid = ({products}) => {
             alt={product.images[0].altText || product.name}
             className="w-full h-full object-cover rounded-lg"
           />
-          <h3 className="text-sm mb-2">{product.name}</h3>
-          <p className="text-gray-500 font-medium text-sm tracking-tighter">
-          ${product.price}
-
-  
-          </p>
-
         </div>
+        <h3 className="text-sm mb-2">{product.name}</h3>
+        <p className="text-gray-500 font-medium text-sm tracking-tighter">
+          ${product.price}
+        </p>
       </div>
     </Link>
   ))}
 </div>
+
 
   )
 }
